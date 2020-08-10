ed28c3f17b4d5cd87d0f76cc9dea8827a068c599
郑思慧 <zhengsihui@zhengsihuideMacBook-Pro.local>
2020-08-10 08:35:49 +0800

    feat: 单链表practice2

--- Linklist/singleLinkList.js
+++ Linklist/singleLinkList.js
@@ -86,4 +86,99 @@ class LinkList {
       currentNode = currentNode.next;
     }
   }
+ 
+  //反转单链表
+  reverseList() {
+    let currentNode = this.head.next;
+    let prevNode = null;
+    while(currentNode !== null) {
+      let nextNode = currentNode.next;
+      currentNode.next = prevNode;
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+    this.head.next = prevNode; 
+  }
+
+  //环验证 
+  //自己想的 有待验证正确性
+  checkCircle1() {
+    let currentNode = this.head.next;
+    while(currentNode !== null && currentNode.next !== this.head) {
+      currentNode = currentNode.next;
+    }
+    return currentNode !== null ? true : false;
+  }
+  //快慢指针 如果是一个循环 快指针走了两个循环最后的位置和慢指针最后的位置一样
+  checkCircle2() {
+    let fast = this.head.next;
+    let slow = this.head;
+    while(fast !== null && fast.next !== null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if(fast === slow) return true;
+    }
+    return false;
+  }
+
+  //删除倒数第k个节点
+  removeByIndexFromEnd(index) {
+    //先判断是否是环
+    if(this.checkCircle2()) return false;
+    this.reverseList();
+    const currenNode = this.findByIndex(index - 1);
+    if(currenNode === null) {
+      console.log('该节点不存在');
+      return false;
+    } 
+    this.remove(currenNode.element);
+    this.reverseList();
+  }
+
+  //求中间节点 利用快慢指针
+  findMiddleNode() {
+    //先判断是否是环
+    if(this.checkCircle2()) return false;
+    let fast = this.head.next;
+    let slow = this.head.next;
+    while(fast.next !== null && fast.next.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+
 }
+
+function mergeSortedLists(listA, listB) {
+  if(!listA) {
+    return listB;
+  }
+  if(!listB) {
+    return listA;
+  }
+  let a = listA;
+  let b = listB;
+  const result = new LinkList();
+  let currentNode = result.head;
+  while(a !== null && b!== null) {
+    if(a.element < b.element) {
+      currentNode.next = a;
+      a = a.next;
+    }
+    else {
+      currentNode.next = b;
+      b = b.next;
+    }
+    currentNode = currentNode.next;
+  }
+  if(a !== null) {
+    currentNode.next = a;
+  }
+  else if (b !== null) {
+    currentNode.next = b;
+  }
+  return result;
+}
+
+
